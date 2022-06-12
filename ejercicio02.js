a10c4ecb26b33a3145b18f27340703c1a40540f6
jig7709 <84993457+jig7709@users.noreply.github.com>
2022-06-12 19:59:10 +0200

    ejercicio02
    Enunciado del ejercicio:
    
    Crea un nuevo archivo JS que contenga una lista con los siguientes elementos:
    
    - Tu nombre (string)
    
    - Tu edad (number)
    
    - ¿Eres desarrollador? (boolean)
    
    - Tu fecha de nacimiento (Date)
    
    - Tu libro favorito (Objeto con propiedades: titulo, autor, fecha, url)

---
+++ ejercicio02.js
@@ -0,0 +1,16 @@
+const nombre = "Georgi Ivanov";
+
+const edad = 44
+
+if (true) {
+    console.log("¿Eres desarollador?");  
+}
+
+const fecha_nac = new Date (1977, 09, 28)
+
+const libro = {
+    titulo: "Mi libro favorito",
+    autor: "Artur Komorowki",
+    fecha: "22 de Marzo 2009",
+    url: "https://miweb.com",
+}
